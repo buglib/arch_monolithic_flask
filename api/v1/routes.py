e364bffdb1ac915da8a8262f1f3d685bb1299e5d
buglib <buglib@foxmail.com>
2022-11-13 23:17:21 +0800

    使用swagger-py-codegen创建Flask项目

---
+++ api/v1/routes.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+###
+### DO NOT CHANGE THIS FILE
+### 
+### The code is auto generated, your change will be overwritten by 
+### code generating.
+###
+from __future__ import absolute_import
+
+from .api.index import Index
+
+
+routes = [
+    dict(resource=Index, urls=['/index'], endpoint='index'),
+]
