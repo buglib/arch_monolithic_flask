f7af5a525304ae26df4ed850d151c20e50375093
buglib <buglib@foxmail.com>
2022-12-08 23:28:55 +0800

    更改项目配置

--- api/settings.py
+++ api/settings.py
@@ -8,8 +8,12 @@ class Config:
     JWT_SIGNATURE_KEY = "601304E0-8AD4-40B0-BD51-0B432DC47461"
     OAUTH2_CLIENT_ID = "bookstore_frontend"
     OAUTH2_CLIENT_SECRET = "bookstore_secret"
-    OAUTH2_ACCESS_TOKEN_EXPIRED_TIME = 3 * 60 * 60
-    OAUTH2_REFRESH_TOKEN_EXPIRED_TIME = 15 * 24 * 60 * 60
+    OAUTH2_ACCESS_TOKEN_EXPIRED_TIME = 3 * 60 * 60 * 1000
+    OAUTH2_REFRESH_TOKEN_EXPIRED_TIME = 15 * 24 * 60 * 60 * 1000
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://bookstore:bookstore@localhost:3306/bookstore"
 
 
 class TestConfig(Config):
@@ -21,7 +25,7 @@ class ProdConfig(Config):
 
 
 configs = dict(
-    default=TestConfig,
+    default=DevConfig,
     dev=TestConfig,
     test=TestConfig,
     prod=ProdConfig
