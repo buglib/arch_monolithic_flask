22464718369777edcb2149cdcc4c9c5fae5d3f58
buglib <buglib@foxmail.com>
2022-12-31 21:22:30 +0800

    重构"/v1/accounts/{username}"的测试用例

--- tests/v1/resources/test_accounts_username.py
+++ tests/v1/resources/test_accounts_username.py
@@ -1,15 +1,14 @@
-from unittest import TestCase
-
 import pytest as pt
 
 from api.v1.models import db, Account
-from tests.fixtures import api_test_client
+from tests.fixtures import testing_app_and_client
 
 
-@pt.mark.usefixtures("api_test_client")
-class AccountsUsernameTestCase(TestCase):
+@pt.mark.usefixtures("testing_app_and_client")
+class TestAccountsUsernameResource:
 
-    def test_return_200(self):
+    def test_return_200(self, testing_app_and_client):
+        _, client = testing_app_and_client
         user = Account(
             username="Messi",
             password="123",
@@ -19,7 +18,7 @@ class AccountsUsernameTestCase(TestCase):
         db.session.add(user)
         db.session.commit()
 
-        resp = self.client.get("/v1/accounts/Messi")
+        resp = client.get("/v1/accounts/Messi")
         assert resp.status_code == 200
         assert resp.json["username"] == "Messi"
         assert resp.json["name"] is None
@@ -28,8 +27,9 @@ class AccountsUsernameTestCase(TestCase):
         assert resp.json["location"] is None
         assert resp.json["avatar"] is None
 
-    def test_return_404(self):
-        resp = self.client.get("/v1/accounts/Luccy")
+    def test_return_404(self, testing_app_and_client):
+        _, client = testing_app_and_client
+        resp = client.get("/v1/accounts/Luccy")
         assert resp.status_code == 404
         assert resp.json["code"] == -1
         assert resp.json["message"] == "按用户名查找用户失败，即用户不存在"
